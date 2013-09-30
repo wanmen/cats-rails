84aac60ae7302e87eb54dd48b2c6435941acad7a
Shao <shaohuan.li@gmail.com>
2013-09-30 17:53:41 +0800

    Added Role Management For Super Admin

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -5,14 +5,41 @@ class HelpController < ApplicationController
   end
   def manage
   	@role = current_user[:role]
+  	if (current_user[:role]== 9)
+  		@users = User.all
+  	end
   end
 
   def apply
     @role = current_user[:role]
     if (@role == 0)
     	current_user[:role]=1
-    	current_user.save
+    	respond_to do |format|
+	    	if current_user.save
+		        format.html { redirect_to :back, notice: '申请成功提交' }
+		    else
+		        format.html { redirect_to :back, notice: '申请失败' }
+		    end
+		end
+    elsif (@role == 9)
+    	@user = User.find(params[:user_id])
+    	@user.role = params[:role]
+    	respond_to do |format|
+	    	if @user.save
+		       format.html { redirect_to :back, notice: '成功修改权限' }
+		    else
+		       format.html { redirect_to :back, notice: '修改权限失败' }
+		    end
+		end
+	elsif (@role == 2)
+		current_user[:role]=3
+    	respond_to do |format|
+	    	if current_user.save
+		        format.html { redirect_to :back, notice: '申请成功提交' }
+		    else
+		        format.html { redirect_to :back, notice: '申请失败' }
+		    end
+		end
     end
-    render :manage
   end
 end
