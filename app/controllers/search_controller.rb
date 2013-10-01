a453a001db5345cc3fcc60e98d4dedb2fd86be85
Shao <shaohuan.li@gmail.com>
2013-10-02 02:46:44 +0800

    Added Basic Search Based on Type and List

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,23 +1,75 @@
 class SearchController < ApplicationController
 	def index
-		@params = params
-		if(params[:title])
-			@title = params[:title]
-			if (params[:type])
-				case params[:type] 
-				when 'book' 
-				  @books = Book.find(:all, :conditions => ["title LIKE ?", "%"+params[:title]+"%"])
-				when 'video' 
-				  @videos =  Video.find(:all, :conditions => ["title LIKE ?", "%"+params[:title]+"%"])
-			    when 'article' 
-				  @articles =  Article.find(:all, :conditions => ["title LIKE ?", "%"+params[:title]+"%"])
-				else # the else clause is optional  
-				end 
-			else 
-				@books = Book.find(:all, :conditions => ["title LIKE ?", "%"+params[:title]+"%"])
-				@videos =  Video.find(:all, :conditions => ["title LIKE ?", "%"+params[:title]+"%"])
-			    @articles =  Article.find(:all, :conditions => ["title LIKE ?", "%"+params[:title]+"%"])
+
+		@books=[]
+		@videos=[]
+		@articles=[]
+		@booklists=[]
+		@videolists=[]
+		@articlelists=[]
+		@mixlists=[]
+		if params[:type]
+			@type = params[:type].split(',')
+			@type.each do |t|
+	 			if (params[:title])
+					case t 
+					when 'book' 
+					  @books += Book.find(:all, :conditions => ["title LIKE ?", "%"+params[:title]+"%"])
+					when 'video' 
+					  @videos +=  Video.find(:all, :conditions => ["title LIKE ?", "%"+params[:title]+"%"])
+				    when 'article' 
+					  @articles +=  Article.find(:all, :conditions => ["title LIKE ?", "%"+params[:title]+"%"])
+					else # the else clause is optional  
+					end 
+				else
+					case t 
+					when 'book' 
+					  @books += Book.find(:all)
+					when 'video' 
+					  @videos +=  Video.find(:all)
+				    when 'article' 
+					  @articles +=  Article.find(:all)
+					else # the else clause is optional  
+					end 
+				end
 			end
 		end
+
+		if params[:list]
+			@list = params[:list].split(',')
+			@list.each do |l|
+	 			if (params[:title])
+					case l 
+					when 'book' 
+					  @booklists += List.where(list_type: 1).find(:all, :conditions => ["title LIKE ?", "%"+params[:title]+"%"])
+					when 'video' 
+					  @videolists +=  List.where(list_type: 2).find(:all, :conditions => ["title LIKE ?", "%"+params[:title]+"%"])
+				    when 'article' 
+					  @articlelists +=  List.where(list_type: 3).find(:all, :conditions => ["title LIKE ?", "%"+params[:title]+"%"])
+				    when 'mix' 
+					  @articlelists +=  List.where(list_type: 4).find(:all, :conditions => ["title LIKE ?", "%"+params[:title]+"%"])
+					else # the else clause is optional  
+					end 
+				else
+					case l 
+					when 'book' 
+					  @booklists += List.where(list_type: 1)
+					when 'video' 
+					  @videolists +=  List.where(list_type: 2)
+				    when 'article' 
+					  @articlelists +=  List.where(list_type: 3)
+					when 'mix' 
+					  @mixlists +=  List.where(list_type: 4)
+					else # the else clause is optional  
+					end 
+				end
+			end
+		end
+		if params[:title]
+			@title = params[:title]
+		else
+			@title = ''
+		end
+		@results = @books+@videos+@articles+@booklists+@videolists+@articlelists+@mixlists
 	end
 end
