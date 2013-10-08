6fbb4c1aea888f7136924a13f173a816c64594f3
andy135790 <andy135790@gmail.com>
2013-10-08 20:14:07 +0800

    add dependency

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,9 @@
 class Article < ActiveRecord::Base
-	has_many :likes, :as => :likeable
-	has_many :taglinks, :as => :tagable
-	has_many :rates, :as => :rateable
-	has_many :comments, :as => :commentable
-	has_many :links, :as => :linkable
+	has_many :likes, :as => :likeable, dependent: :destroy
+	has_many :taglinks, :as => :tagable, dependent: :destroy
+	has_many :rates, :as => :rateable, dependent: :destroy
+	has_many :comments, :as => :commentable, dependent: :destroy
+	has_many :links, :as => :linkable, dependent: :destroy
 	belongs_to :user
   scope :best6, ->{ select("articles.id, articles.title, articles.summary, articles.user_id, articles.updated_at, count(rates.id) AS rates_count").joins(:rates).group("articles.id").order("rates_count DESC").limit(6)}
   scope :newest10, ->{ select("articles.id,articles.title").order("created_at desc").limit(10)}
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,10 +1,10 @@
 class List < ActiveRecord::Base
   belongs_to :user
-  has_many :taglinks, :as => :tagable
-  has_many :likes, :as => :likeable
-  has_many :comments, :as => :commentable
-  has_many :rates, :as => :rateable
-  has_many :links
+  has_many :taglinks, :as => :tagable, dependent: :destroy
+  has_many :likes, :as => :likeable, dependent: :destroy
+  has_many :comments, :as => :commentable, dependent: :destroy
+  has_many :rates, :as => :rateable, dependent: :destroy
+  has_many :links, dependent: :destroy
   serialize :links_array, Array
   scope :top10Booklist,->{select("lists.id, lists.title, count(likes.id) AS likes_count").where(list_type: 1).joins(:likes).group("lists.id").order("likes_count DESC").limit(10)}
   scope :newest3Booklist,->{select("lists.id, lists.title").where(list_type: 1).order("created_at desc").limit(3)}
