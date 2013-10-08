024001ae3c583e0fc1af359254dff9de441755ef
XTao <xutao881001@gmail.com>
2013-10-09 01:19:29 +0800

    Fix images.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Cats::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( markdown.js ie.js raty.js bootstrap-ie7.css bootstrap.css wm-default.css wm-pagedown.css search.js )
+  config.assets.precompile += %w( markdown.js ie.js raty.js bootstrap-ie7.css bootstrap.css wm-default.css wm-pagedown.css search.js rails.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
