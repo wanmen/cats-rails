a83e2c0fbb74f1da2dcc89d8bf784e0e5912ed6d
Shao <shaohuan.li@gmail.com>
2013-10-24 14:44:49 +0800

    Added Cover to Lists

--- app/models/list.rb
+++ app/models/list.rb
@@ -6,10 +6,11 @@ class List < ActiveRecord::Base
   has_many :rates, :as => :rateable, dependent: :destroy
   has_many :links, dependent: :destroy
   serialize :links_array, Array
-  scope :top10Booklist,->{select("lists.id, lists.title, count(likes.id) AS likes_count").where(list_type: 1).joins(:likes).group("lists.id").order("likes_count DESC").limit(10)}
-  scope :newest3Booklist,->{select("lists.id, lists.title").where(list_type: 1).order("created_at desc").limit(3)}
-  scope :top10Videolist,->{select("lists.id, lists.title, count(likes.id) AS likes_count").where(list_type: 2).joins(:likes).group("lists.id").order("likes_count DESC").limit(10)}
-  scope :newest6Videolist,->{select("lists.id, lists.title").where(list_type: 2).order("created_at desc").limit(6)}
-  scope :best6Booklist,->{select("lists.id, lists.title, lists.rate").where(list_type: 1).group("lists.id").order("lists.rate DESC").limit(6)}
-  scope :best6Videolist,->{select("lists.id, lists.title, lists.rate").where(list_type: 2).group("lists.id").order("lists.rate DESC").limit(6)}
+  mount_uploader :cover, ImageUploader
+  scope :top10Booklist,->{select("lists.id, lists.title, lists.cover, count(likes.id) AS likes_count").where(list_type: 1).joins(:likes).group("lists.id").order("likes_count DESC").limit(10)}
+  scope :newest3Booklist,->{select("lists.id, lists.cover, lists.title").where(list_type: 1).order("created_at desc").limit(3)}
+  scope :top10Videolist,->{select("lists.id, lists.cover, lists.title, count(likes.id) AS likes_count").where(list_type: 2).joins(:likes).group("lists.id").order("likes_count DESC").limit(10)}
+  scope :newest6Videolist,->{select("lists.id,lists.cover, lists.title").where(list_type: 2).order("created_at desc").limit(6)}
+  scope :best6Booklist,->{select("lists.id, lists.cover, lists.title, lists.rate").where(list_type: 1).group("lists.id").order("lists.rate DESC").limit(6)}
+  scope :best6Videolist,->{select("lists.id, lists.cover, lists.title, lists.rate").where(list_type: 2).group("lists.id").order("lists.rate DESC").limit(6)}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131019043354) do
+ActiveRecord::Schema.define(version: 20131024060524) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -126,6 +126,7 @@ ActiveRecord::Schema.define(version: 20131019043354) do
     t.text     "links_array"
     t.float    "rate",        default: 0.0
     t.integer  "popularity",  default: 0
+    t.string   "cover"
   end
 
   add_index "lists", ["user_id"], name: "index_lists_on_user_id", using: :btree
